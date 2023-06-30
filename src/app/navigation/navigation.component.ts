96aee63b677601d9d3cadd191118434c6cfe763a
nonnodacciaio <nonnodacciaio@gmail.com>
2023-06-30 09:46:45 +0200

    Update navigation.component.ts
    Made home the default link

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,6 +7,8 @@ import { Router } from "@angular/router";
 		<div class="flex justify-between items-center m-3">
 			<div class="flex">
 				<a
+          routerLinkActive="home" 
+          [routerLinkActiveOptions]="{ exact: true, isActive: true }
 					[color]="isActive('home') ? 'accent' : 'primary'"
 					mat-button
 					[routerLink]="'home'">
