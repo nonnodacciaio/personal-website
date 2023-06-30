707100e8c55104c968e2f4602d2da69eb15c6df9
nonnodacciaio <nonnodacciaio@gmail.com>
2023-06-30 19:17:28 +0200

    Added icons to contacts page

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -6,8 +6,20 @@ import { Component } from "@angular/core";
 		<h3 class="text-amber-300">Contacts</h3>
 		<p>Feel free to get in touch with me using the following methods:</p>
 		<ul>
-			<li>Email: marco.sabau98@gmail.com</li>
-			<li>GitHub: github.com/nonnodacciaio</li>
+			<li class="m-3"><i class="fa-solid fa-envelope"></i>&nbsp;marco.sabau98@gmail.com</li>
+			<li class="m-3">
+				<i class="fa-brands fa-github"></i>&nbsp;<a
+					class="text-amber-300 hover:underline"
+					href="https://www.github.com/nonnodacciaio"
+					>github.com/nonnodacciaio</a
+				>&nbsp;<a
+					href="https://www.github.com/nonnodacciaio"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-amber-300 hover:underline"
+					><i class="fas fa-up-right-from-square"></i
+				></a>
+			</li>
 		</ul>
 	</div>`
 })
